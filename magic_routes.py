570c0f7b17813f8e3d490756458d826bb69e8c4e
Pedro Tortella <pedro.tortella@gmail.com>
2017-11-25 21:08:43 -0200

    Adding /card/:card_id route implementation

--- magic_routes.py
+++ magic_routes.py
@@ -2,6 +2,8 @@ from flask import Flask, abort  # Routes
 import db  # MySQL
 import rabbit_mq as mq  #rabiit_mq
 import json
+from sys import platform
+import getpass
 
 app = Flask(__name__)
 
@@ -34,4 +36,16 @@ def move_all():
 
 @app.route('/card/<int:card_id>', methods=['GET'])
 def get_card(card_id):
-    return "Card id: {}".format(card_id)
+    if platform == "linux" or platform == "linux2":
+        file = open("/tmp/cards_db.txt", "r")
+    elif platform == "win32":
+        file = open("C:\\Users\\{}\\AppData\\Local\\Temp\\cards_db.txt".format(getpass.getuser()), "r")
+    data = file.readlines()
+    file.close()
+    for card in data:
+        card_json = json.loads(card)
+        card_id = str(card_id)
+        if card_json.get('GathererId') == card_id:
+            return json.dumps(card_json)
+    # Card Id not found
+    abort(404)
