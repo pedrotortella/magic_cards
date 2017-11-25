42478f8d8c86df40385403174bdc20b7fd31078e
Pedro Tortella <pedro.tortella@gmail.com>
2017-11-25 20:19:36 -0200

    Consumer writing on file

--- consumer.py
+++ consumer.py
@@ -1,8 +1,18 @@
 import rabbit_mq as mq
+from sys import platform
+import getpass
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    if platform == "linux" or platform == "linux2":
+        with open("/tmp/cards_db.txt", "a") as file:
+            file.write(body.decode('UTF-8') + "\n")
+            file.close()
+    elif platform == "win32":
+        with open("C:\\Users\\{}\\AppData\\Local\\Temp\\cards_db.txt".format(getpass.getuser()), "a") as file:
+            file.write(body.decode('UTF-8') + "\n")
+            file.close()
+    print(" [x] Received %r" % body.decode('UTF-8'))
 
 
 mq_conn = mq.rabbit_mq()
